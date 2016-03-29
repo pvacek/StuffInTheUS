4393189f08cf7a7c20e2cd85db0d94194f75af61
pvacek <prvacek@ucdavis.edu>
2016-03-28 18:30:47 -0700

    added a run script

---
+++ RunApp.R
@@ -0,0 +1,8 @@
+#Run the app, install any packages if needed
+
+#packages<-c("ggplot2","dplyr","ggthemes","shiny")
+#install.packages(packages)
+
+source("https://raw.githubusercontent.com/pvacek/StuffInTheUS/master/ScriptFiles/ui.R")
+source("https://raw.githubusercontent.com/pvacek/StuffInTheUS/master/ScriptFiles/server.R")
+runApp()
