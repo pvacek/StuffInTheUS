d00f977e9d02c1e00b7f0eca7d85d74646275c47
pvacek <prvacek@ucdavis.edu>
2016-03-28 18:35:16 -0700

    updated RunApp.R

--- RunApp.R
+++ RunApp.R
@@ -3,6 +3,10 @@
 #packages<-c("ggplot2","dplyr","ggthemes","shiny")
 #install.packages(packages)
 
-source("https://raw.githubusercontent.com/pvacek/StuffInTheUS/master/ScriptFiles/ui.R")
-source("https://raw.githubusercontent.com/pvacek/StuffInTheUS/master/ScriptFiles/server.R")
-runApp()
+ui<-"https://raw.githubusercontent.com/pvacek/StuffInTheUS/master/ScriptFiles/ui.R"
+server<-"https://raw.githubusercontent.com/pvacek/StuffInTheUS/master/ScriptFiles/server.R"
+
+source(ui)
+source(server)
+
+runApp(list(ui,server))
